7e3513262971bd9a2283a15b80d50f1146e47bf2
Steven <06dm20@gmail.com>
2025-05-12 11:53:39 -0500

    Analisis

--- TareasLaura/src/index.js
+++ TareasLaura/src/index.js
@@ -8,6 +8,6 @@ app.use(tasksControllers);
 require('dotenv').config();
 PORT = 8002
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Tareas escuchando desde el puerto ${PORT}`);
 });
